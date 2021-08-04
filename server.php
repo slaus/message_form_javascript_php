c70f734efb6fa462cc2f6678463e0ad9c3b1585d
Vyacheslav <mister.slaus@gmail.com>
2021-08-04 12:04:29 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,3 +1,3 @@
 <?php
-$_POST = json_decode( file_get_contents("php://input"), true ); //If use JSON. Else need to comment it string.
+$_POST = json_decode( file_get_contents("php://input"), true ); //If use JSON. Else need to comment this string.
 echo var_dump($_POST);
